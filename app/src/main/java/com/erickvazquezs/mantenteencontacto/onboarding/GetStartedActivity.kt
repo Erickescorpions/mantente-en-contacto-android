a12c74743507d6b35785b33507c235330c4d54c0
Erickescorpions <erickescorpions@hotmail.com>
2025-09-15 22:24:31 -0600

    i18n: add Spanish and English translations

--- app/src/main/java/com/erickvazquezs/mantenteencontacto/onboarding/GetStartedActivity.kt
+++ app/src/main/java/com/erickvazquezs/mantenteencontacto/onboarding/GetStartedActivity.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.erickvazquezs.mantenteencontacto.R
 
-class GetStartActivity : AppCompatActivity() {
+class GetStartedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_get_start)
+        setContentView(R.layout.activity_get_started)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

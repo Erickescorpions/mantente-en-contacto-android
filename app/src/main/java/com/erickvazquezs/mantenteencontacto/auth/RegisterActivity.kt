e115191059544ad10e0ae78a3071c566d49cf4ed
Erickescorpions <erickescorpions@hotmail.com>
2025-09-16 12:16:49 -0600

    feat: include avatar selection

--- app/src/main/java/com/erickvazquezs/mantenteencontacto/auth/RegisterActivity.kt
+++ app/src/main/java/com/erickvazquezs/mantenteencontacto/auth/RegisterActivity.kt
@@ -1,17 +1,31 @@
 package com.erickvazquezs.mantenteencontacto.auth
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.erickvazquezs.mantenteencontacto.R
+import com.erickvazquezs.mantenteencontacto.avatar.ChooseAvatarActivity
 import com.erickvazquezs.mantenteencontacto.databinding.ActivityRegisterBinding
+import com.erickvazquezs.mantenteencontacto.models.AvatarEntity
 
 class RegisterActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityRegisterBinding
+    val errors = mutableListOf<String>()
+
+    private val register =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val avatar = result.data?.getSerializableExtra("EXTRA_AVATAR_KEY") as AvatarEntity
+                binding.imgAvatar.setImageResource(avatar.avatarId)
+            } else {
+                Toast.makeText(this, "RESULT_CANCELLED", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,12 +43,29 @@ class RegisterActivity : AppCompatActivity() {
         // por defecto la imagen sera img1
         binding.imgAvatar.setImageResource(R.drawable.img1)
 
+        binding.imgAvatar.setOnClickListener {
+            val intent = Intent(this, ChooseAvatarActivity::class.java)
+            register.launch(intent)
+        }
 
-
-
+//        binding.btnCreate.setOnClickListener {
+//            if(!validate()) {
+//                // mostramos los errores en la lista
+//
+//
+//                return
+//            }
+//
+//            // si se valida la informacion
+//            val intent = Intent(this, )
+//        }
 
         binding.btnGoogle.setOnClickListener {
             Toast.makeText(this, R.string.coming_soon, Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun validate(): Boolean {
+        return true
     }
 }
---
+++ app/src/main/java/com/erickvazquezs/mantenteencontacto/models/AvatarEntity.kt
@@ -0,0 +1,7 @@
+package com.erickvazquezs.mantenteencontacto.models
+
+import java.io.Serializable
+
+data class AvatarEntity(
+    val avatarId: Int
+): Serializable

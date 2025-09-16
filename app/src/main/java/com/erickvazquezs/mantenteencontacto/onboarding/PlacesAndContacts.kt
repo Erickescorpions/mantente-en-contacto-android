1401d88a94989d89232e8734aff5aebedf185fff
Erickescorpions <erickescorpions@hotmail.com>
2025-09-15 19:56:06 -0600

    feat: add onboarding views for the app

---
+++ app/src/main/java/com/erickvazquezs/mantenteencontacto/onboarding/PlacesAndContacts.kt
@@ -0,0 +1,21 @@
+package com.erickvazquezs.mantenteencontacto.onboarding
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.erickvazquezs.mantenteencontacto.R
+
+class PlacesAndContacts : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_places_and_contacts)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}

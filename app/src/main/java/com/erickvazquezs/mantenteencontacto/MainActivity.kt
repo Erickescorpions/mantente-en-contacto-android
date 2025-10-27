c8e00909da983f9f630ec9cc4c63b5fe335ef029
Erickescorpions <erickescorpions@hotmail.com>
2025-10-26 23:32:56 -0600

    styles: update onboarding UI colors and layout

--- app/src/main/java/com/erickvazquezs/mantenteencontacto/MainActivity.kt
+++ app/src/main/java/com/erickvazquezs/mantenteencontacto/MainActivity.kt
@@ -14,14 +14,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, 0, systemBars.right, 0)
             insets
         }
 

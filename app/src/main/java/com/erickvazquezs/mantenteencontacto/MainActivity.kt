688ef1eb3e7b4599a7b0da53a7022ce2216004ee
Erickescorpions <erickescorpions@hotmail.com>
2025-10-27 23:16:02 -0600

    feat: implement Navigation Component

--- app/src/main/java/com/erickvazquezs/mantenteencontacto/MainActivity.kt
+++ app/src/main/java/com/erickvazquezs/mantenteencontacto/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.erickvazquezs.mantenteencontacto.databinding.ActivityMainBinding
 import com.erickvazquezs.mantenteencontacto.ui.fragments.onboarding.MainOnboardingFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +26,10 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        supportFragmentManager.beginTransaction().replace(
-            R.id.container,
-            MainOnboardingFragment()
-        ).commit()
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.container
+        ) as NavHostFragment
+
+        navController = navHostFragment.navController
     }
 }
--- app/src/main/java/com/erickvazquezs/mantenteencontacto/ui/fragments/onboarding/GetStartedFragment.kt
+++ app/src/main/java/com/erickvazquezs/mantenteencontacto/ui/fragments/onboarding/GetStartedFragment.kt
@@ -5,15 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.erickvazquezs.mantenteencontacto.R
+import com.erickvazquezs.mantenteencontacto.databinding.FragmentGetStartedBinding
 
 class GetStartedFragment : Fragment() {
+
+    private var _binding: FragmentGetStartedBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnCreateAccount.setOnClickListener {
+            findNavController().navigate(R.id.action_mainOnboardingFragment2_to_registerFragment)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_get_started, container, false)
+
+        _binding = FragmentGetStartedBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }

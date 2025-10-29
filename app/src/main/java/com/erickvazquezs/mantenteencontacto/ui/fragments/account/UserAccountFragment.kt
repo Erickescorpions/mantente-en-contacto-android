288fdfd07e6ada150c0908d184f75ee873ffb898
Erickescorpions <erickescorpions@hotmail.com>
2025-10-28 23:46:23 -0600

    chore: remove unused activities

--- app/src/main/java/com/erickvazquezs/mantenteencontacto/ui/fragments/account/UserAccountFragment.kt
+++ app/src/main/java/com/erickvazquezs/mantenteencontacto/ui/fragments/account/UserAccountFragment.kt
@@ -6,13 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.erickvazquezs.mantenteencontacto.R
+import com.erickvazquezs.mantenteencontacto.databinding.FragmentUserAccountBinding
 
 class UserAccountFragment : Fragment() {
+
+    private var _binding: FragmentUserAccountBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_account, container, false)
+
+        _binding = FragmentUserAccountBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }

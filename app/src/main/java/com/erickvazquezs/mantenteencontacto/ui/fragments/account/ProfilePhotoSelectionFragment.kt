9288ce75318535113bdf6910841d4c5c80042328
Erickescorpions <erickescorpions@hotmail.com>
2025-12-15 23:30:38 -0600

    feat: layout of user pp selection

---
+++ app/src/main/java/com/erickvazquezs/mantenteencontacto/ui/fragments/account/ProfilePhotoSelectionFragment.kt
@@ -0,0 +1,28 @@
+package com.erickvazquezs.mantenteencontacto.ui.fragments.account
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.erickvazquezs.mantenteencontacto.R
+import com.erickvazquezs.mantenteencontacto.databinding.FragmentProfilePhotoSelectionBinding
+
+class ProfilePhotoSelectionFragment : Fragment() {
+
+    private var _binding: FragmentProfilePhotoSelectionBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentProfilePhotoSelectionBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

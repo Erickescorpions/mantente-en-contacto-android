47126a9c2e1b29d9f9f4dc05b9aac4f91a381c7e
Erickescorpions <erickescorpions@hotmail.com>
2025-12-19 10:57:30 -0600

    feat: setup inicial de firebase cloud messaging

---
+++ app/src/main/java/com/erickvazquezs/mantenteencontacto/utils/notifications/MyFirebaseMessagingService.kt
@@ -0,0 +1,7 @@
+package com.erickvazquezs.mantenteencontacto.utils.notifications
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService: FirebaseMessagingService() {
+
+}
